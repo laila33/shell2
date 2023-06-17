811948c9a79f603b848f5c867131756f3b3b2013
laila33 <lailaabdelnaser64@gmail.com.com>
2023-06-17 07:33:17 -0700

    a

---
+++ string_function_3.c
@@ -0,0 +1,87 @@
+#include "shell_.h"
+
+/**
+ **strncpy_2 - copy the  string.
+ *@s2: destination string to copy.
+ *
+ *@s1: the source string.
+ *
+ *@num: the number  of characters to copy.
+ *
+ *Return: string after concatenated.
+ */
+char strncpy_2(char *s2, char *s1, int num)
+{
+	int x = 0, y;
+	char *c = s2;
+
+	while (s1[x] != '\0' && x < num - 1)
+	{
+		s2[x] = s1[x];
+		x++;
+	}
+
+	if (x < num)
+	{
+		x = y;
+		while (y < num)
+		{
+			s2[j] = '\0';
+		}
+	}
+	return (c);
+}
+
+
+/**
+ **strncat_2 - concatenate two strings.
+ *
+ *
+ *@s2: first string.
+ *
+ *@s1: the second string.
+ *@num: the number of bytes to  use.
+ *
+ *Return: concatenated string.
+ */
+char strncat_2(char *s2, char *s1, int num)
+{
+	int x = 0, y = 0;
+	char *c = s2;
+
+
+	while (s2[x] != '\0')
+		x++;
+	while (s1[y] != '\0' && y < num)
+	{
+		s2t[x] = s1[y];
+		x++;
+		y++;
+	}
+	if (y < num)
+		s2[x] = '\0';
+	return (c);
+}
+
+
+
+
+
+/**
+ **strchr_2 - find  character in  string.
+ *
+ *@s1: the string to search in.
+ *
+ *@charr: character to find.
+ *
+ *Return:  pointer to the memory area s1.
+ */
+char strchr_2(char *s1, char charr)
+{
+	do {
+		if (*s1 == charr)
+			return (s1);
+	} while (*s1++ != '\0');
+
+	return (NULL);
+}
